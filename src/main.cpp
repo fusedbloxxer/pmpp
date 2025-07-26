bf977f64b0420462df845d9d6dac4f06cc279202
Andrei Ionescu <voidwarden@yahoo.com>
2025-07-26 14:25:57 +0300

    Use libtorch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <filesystem>
 #include <iostream>
+#include <torch/torch.h>
 
 #include "./chapter_02/chapter_02.h"
 #include "./chapter_03/chapter_03.h"
@@ -9,7 +10,9 @@ namespace fs = std::filesystem;
 
 int main()
 {
-    chapter_03::sample::blur_kernel::main();
+    torch::Tensor tensor = torch::rand({2, 3}).to(torch::kCUDA);
+
+    std::cout << tensor << std::endl;
 
     return 0;
 }
